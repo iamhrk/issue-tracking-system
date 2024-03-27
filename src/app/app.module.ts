e8c573b1e74ca7d7e1e700692478f03bd4c19a3a
iamhrk <iamhrk8@gmail.com>
2024-03-27 22:31:11 +0530

    create issues service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from
+  '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
